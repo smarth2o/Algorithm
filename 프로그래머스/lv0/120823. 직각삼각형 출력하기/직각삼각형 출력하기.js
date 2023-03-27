a0d8ee5a3d7c0f0c47a6627cc46374cfe7f3bbc2
smarth2o <69911502+smarth2o@users.noreply.github.com>
2023-03-27 15:56:42 +0900

    [level 0] Title: 직각삼각형 출력하기, Time: 42.83 ms, Memory: 32.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.js
@@ -0,0 +1,15 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = "";
+
+rl.on('line', function (line) {
+    input = line;
+}).on('close', function () {
+    for (var i=1; i <= Number(input); i++) {
+        console.log('*'.repeat(i));
+    }
+});
