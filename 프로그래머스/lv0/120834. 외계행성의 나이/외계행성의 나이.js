548d5d4aaed79ca4fc8ed17a5ec460ff900e2f04
smarth2o <69911502+smarth2o@users.noreply.github.com>
2023-03-28 16:52:22 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -1,4 +1,4 @@
 function solution(age) {
     var planet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
-    return Array.from(String(age), (str) => Number(str)).map(num => planet[num]).join("");
+    return Array.from(age.toString()).map(num => planet[num]).join("");
 }
