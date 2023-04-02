fc10ce2362275ad28aa2ef97e32db9113679ee5f
smarth2o <69911502+smarth2o@users.noreply.github.com>
2023-04-02 18:31:50 +0900

    [level 0] Title: 구슬을 나누는 경우의 수, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.js
+++ 프로그래머스/lv0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.js
@@ -1,10 +1,9 @@
 function fact(num) {
-    var num = BigInt(num);
-    if (num === BigInt(1) || num === BigInt(0)) {
-        return BigInt(1);
+    if (num === 1 || num === 0) {
+        return 1;
     }
-    return num * fact(num - BigInt(1));
+    return num * fact(num - 1);
 }
 function solution(balls, share) {
-    return fact(balls) / (fact(balls-share) * fact(share));
+    return Math.round(fact(balls) / (fact(balls-share) * fact(share)));
 }
