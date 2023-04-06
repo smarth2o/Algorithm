1f0c0163dc8fcfdcc8600357deda61da87d11d7e
smarth2o <69911502+smarth2o@users.noreply.github.com>
2023-04-06 23:41:30 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.26 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -0,0 +1,9 @@
+function solution(my_string) {
+    return my_string.split("").map(v => {
+        if (v.toLowerCase() === v) {
+            return v.toUpperCase();
+        } else {
+            return v.toLowerCase();
+        }
+    }).join("");
+}
