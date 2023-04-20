58cff793b69793ad1ee8d29d56a95b94d28db331
smarth2o <69911502+smarth2o@users.noreply.github.com>
2023-04-21 00:31:38 +0900

    [level 0] Title: 평행, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120875. 평행/평행.js
+++ 프로그래머스/lv0/120875. 평행/평행.js
@@ -1,10 +1,10 @@
 function solution(dots) {
     let [a, b, c, d] = dots;
-    if (Math.abs((b[0]-a[0])/(b[1]-a[1])) === Math.abs((d[0]-c[0])/(d[1]-c[1]))) {
+    if ((b[0]-a[0])/(b[1]-a[1]) === (d[0]-c[0])/(d[1]-c[1])) {
         return 1;
-    } else if (Math.abs((c[0]-a[0])/(c[1]-a[1])) === Math.abs((d[0]-b[0])/(d[1]-b[1]))) {
+    } else if ((c[0]-a[0])/(c[1]-a[1]) === (d[0]-b[0])/(d[1]-b[1])) {
         return 1;
-    } else if (Math.abs((d[0]-a[0])/(d[1]-a[1])) === Math.abs((b[0]-c[0])/(b[1]-c[1]))) {
+    } else if ((d[0]-a[0])/(d[1]-a[1]) === (b[0]-c[0])/(b[1]-c[1])) {
         return 1;
     }
     return 0;
