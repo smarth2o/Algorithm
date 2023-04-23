e68bfa8186e7790be3d3b5c31afb8d97bee85f30
smarth2o <69911502+smarth2o@users.noreply.github.com>
2023-04-23 15:24:41 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,6 +1,3 @@
 function solution(s) {
-    if ((s.length===4 || s.length===6) && s.split('').filter(v => isNaN(v)).length === 0) {
-        return true;
-    }
-    return false;
+    return (s.length===4 || s.length===6) && s.split('').filter(v => isNaN(v)).length === 0;
 }
