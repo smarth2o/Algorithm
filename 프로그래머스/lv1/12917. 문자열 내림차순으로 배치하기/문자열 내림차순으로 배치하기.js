4ca317abed384fb38089b988c3fb24fc50377c74
smarth2o <69911502+smarth2o@users.noreply.github.com>
2023-04-23 14:59:28 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
+++ 프로그래머스/lv1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
@@ -1,3 +1,3 @@
 function solution(s) {
-    return s.split('').filter(v => v === v.toLowerCase()).sort().reverse().join('') + s.split('').filter(v => v === v.toUpperCase()).sort().reverse().join('');
+    return s.split('').sort().reverse().join('');
 }
