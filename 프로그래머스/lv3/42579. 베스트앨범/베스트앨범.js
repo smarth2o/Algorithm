d9427685972a59479a648f01f9a8e92174898677
smarth2o <69911502+smarth2o@users.noreply.github.com>
2023-05-13 18:12:21 +0900

    [level 3] Title: 베스트앨범, Time: 0.18 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.js
@@ -0,0 +1,24 @@
+function solution(genres, plays) {
+    let songs = {};
+    
+    for (let genre of [...new Set(genres)]) {
+        songs[genre] = [];
+    }
+    for (let i=0; i < genres.length; i++) {
+        songs[genres[i]].push(plays[i]);
+    }
+    
+    Object.values(songs).forEach(v => v.sort((a,b) => b-a));
+    songs = Object.fromEntries(Object.entries(songs).sort(([, a], [, b]) => b.reduce((acc, cur) => acc+cur, 0) - a.reduce((acc, cur) => acc+cur, 0)));
+    
+    let answer = [];
+    for (const [k, v] of Object.entries(songs)) {
+        answer.push(plays.indexOf(v[0]));
+        // 장르에 속한 곡이 하나라면, 하나의 곡만 선택
+        if (plays.indexOf(v[1]) >= 0) {
+            if (v[0]===v[1]) answer.push(plays.indexOf(v[1], answer.at(-1)+1));
+            else answer.push(plays.indexOf(v[1]));
+        }
+    }
+    return answer;
+}
