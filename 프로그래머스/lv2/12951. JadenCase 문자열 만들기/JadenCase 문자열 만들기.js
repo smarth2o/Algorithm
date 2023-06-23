aa0e98ff5f595417edccad39a50c132e5668df5d
smarth2o <69911502+smarth2o@users.noreply.github.com>
2023-06-23 20:13:55 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,11 +1,5 @@
 function solution(s) {
-    let answer = [s[0].toUpperCase()];
-    for (let i=1; i < s.length; i++) {
-        if (answer[answer.length-1] === " ") {
-            answer.push(s[i].toUpperCase());
-        } else {
-            answer.push(s[i].toLowerCase());
-        }
-    }
-    return answer.join('');
+    return s.split(' ').map((v, i) => {
+        if (v!=='') return v[0].toUpperCase() + v.substring(1).toLowerCase();
+    }).join(' ');
 }
