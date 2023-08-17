f13868fd3341831fca7d7da21860b41bc74e3acb
Hyeonjeong Kim <69911502+smarth2o@users.noreply.github.com>
2023-08-17 09:48:59 +0900

    [Silver II] Title: 최대 힙, Time: 328 ms, Memory: 26256 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.java
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -9,11 +9,14 @@ public class Main {
         int N = Integer.parseInt(in.readLine());
 
         PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        StringBuffer sb = new StringBuffer();
         for(int i=0; i<N; i++) {
             int x = Integer.parseInt(in.readLine());
             if (x > 0) maxHeap.offer(x);
-            else if (maxHeap.isEmpty()) System.out.println(0);
-            else System.out.println(maxHeap.poll());
+            else if (maxHeap.isEmpty()) sb.append('0').append('\n');
+            else sb.append(maxHeap.poll()).append('\n');
         }
+
+        System.out.println(sb.toString());
     }
 }
