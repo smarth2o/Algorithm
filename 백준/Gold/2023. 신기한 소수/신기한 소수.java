853b58433e90738027315d78afeec9df37b27677
Hyeonjeong Kim <69911502+smarth2o@users.noreply.github.com>
2023-08-17 17:48:32 +0900

    [Gold V] Title: 신기한 소수, Time: 124 ms, Memory: 14104 KB -BaekjoonHub

---
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    static int N;
+    static String num;
+    static StringBuilder sb;
+    public static void main(String[] args) throws Exception {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        sb = new StringBuilder();
+        for(int x=2; x<=9; x++) {
+            prime(x);
+        }
+        System.out.println(sb.toString());
+    }
+
+    private static void prime(int x) {
+
+        for(int i=2; i*i<=x; i++) {
+            if (x % i == 0) return;
+        }
+        if (Integer.toString(x).length() == N) {
+            sb.append(Integer.toString(x)).append('\n');
+            return;
+        }
+        for(int j=1; j<=9; j++) {
+            prime(x*10+j);
+        }
+    }
+}
