ad28e2e083ec5e39b118b61e63b8d5fea82a8d75
Hyeonjeong Kim <69911502+smarth2o@users.noreply.github.com>
2023-09-07 15:44:07 +0900

    [D4] Title: 염라대왕의 이름 정렬, Time: 3,902 ms, Memory: 185,780 KB -BaekjoonHub

--- SWEA/D4/7701. 염라대왕의 이름 정렬/염라대왕의 이름 정렬.py
+++ SWEA/D4/7701. 염라대왕의 이름 정렬/염라대왕의 이름 정렬.py
@@ -1,9 +1,7 @@
 T = int(input())
 for tc in range(1, T+1):
     N = int(input())
-    names = set()
-    for i in range(N):
-        names.add(input())
+    names = set([input() for _ in range(N)])
     names = sorted(names, key=lambda x: (len(x), x))
     print("#"+str(tc))
     for name in names:
